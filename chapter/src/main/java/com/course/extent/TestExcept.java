dd9baf61133285d4cd0b5686a5858a9d10171d62
Zw <pyj.moon@gmail.com>
2021-06-06 22:34:35 +0800

    testng注解02

--- chapter/src/main/java/com/course/extent/TestExcept.java
+++ chapter/src/main/java/com/course/extent/TestExcept.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class TestExcep {
+public class TestExcept {
     @Test
     public void test1(){
         Assert.assertEquals(1,10);
