04482d040f3a8566a930573300abf020f1378f79
Zw <pyj.moon@gmail.com>
2021-06-06 22:31:06 +0800

    testng注解02

---
+++ chapter/src/main/java/com/course/extent/TestExcep.java
@@ -0,0 +1,23 @@
+package com.course.extent;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestExcep {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,10);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals("test","test");
+    }
+
+    @Test
+    public void test3(){
+        Reporter.log("这是自己写的日志");
+        throw new RuntimeException("这是我们自己运行时异常");
+    }
+}
