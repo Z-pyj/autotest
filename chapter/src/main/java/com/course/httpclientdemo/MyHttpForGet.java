839ec7729a1faae8bf5cdef27b7430c19d3426c0
Zw <pyj.moon@gmail.com>
2021-06-10 22:43:13 +0800

    testng注解02

--- chapter/src/main/java/com/course/httpclientdemo/MyHttpForGet.java
+++ chapter/src/main/java/com/course/httpclientdemo/MyHttpForGet.java
@@ -1,5 +1,56 @@
 package com.course.httpclientdemo;
 
-public class MyHttpForGet {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultHttpClientConnectionOperator;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MyHttpForGet {
+    private String url;
+    //用来读取配置信息
+    private ResourceBundle bundle;
+
+    @BeforeMethod
+    public void beforeTest(){
+        //读取配置文件
+        bundle = ResourceBundle.getBundle("application");
+        //获取url
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        //拼接url
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = this.url+uri;
+        HttpGet get = new HttpGet(testUrl);
+        //用来执行get方法
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        //转string，后接收
+        result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+        //获取cookies信息
+        //获取一个对象，叫 CookieStore，它把所有的信息都记录下来了
+        CookieStore store = client.getCookieStore();
+        //获取cookie信息，返回cookies的泛型，类型为Cookie
+        List<Cookie> cookieList = store.getCookies();
+        //循环读取cookie
+        for(Cookie cookie:cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name="+name+"\tcookie value="+value);
+        }
+    }
 }
